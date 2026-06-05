6d9d04f32513d41a144d3d2e56056e5be24c12bd
Kartik Bhamare <kartikbhamare04@gmail.com>
2026-06-05 14:29:33 +0530

    changes

--- smart_expense_tracker/settings.py
+++ smart_expense_tracker/settings.py
@@ -138,16 +138,16 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STORAGES = {
     'default': {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
